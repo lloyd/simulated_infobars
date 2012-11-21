368ed32595a1531f03c0a6cd77c41ffca9db6691
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-11-21 09:42:28 -0700

    doc improvement

--- extension_api.js
+++ extension_api.js
@@ -1,7 +1,7 @@
 // an interface that can be used to run infobars in extension code.
-// This can be used from page actions, or background javascript, and
-// will forward calls to the "InfobarManager" which runs as persistent
-// background javascript in your extension.
+// This can be used from page actions, browser actions, or background
+// javascript, and will forward calls to the "InfobarManager" which
+// runs as persistent background javascript in your extension.
 //
 // See manager.js for API documentation, as all of the functions exported
 // there are available via SimulatedInfobar.
